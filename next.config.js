246262637dfdbc9c4edfce5253a415a4ed8c01f0
Saina <chimsaina98@gmail.com>
2025-10-16 10:01:34 +0700

    update:favicon.ico

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
-// next.config.js
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.tsx',
+  themeConfig: './theme.config.tsx'
 });
-
 module.exports = withNextra();
