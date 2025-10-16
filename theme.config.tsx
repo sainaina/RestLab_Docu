821fdb593f03bfd455bc6307c8e902a4009ba6ef
Saina <chimsaina98@gmail.com>
2025-10-16 08:40:52 +0700

    update:icon tab and title

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,21 +2,25 @@ import React from "react";
 import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
-  logo: (
+  head: (
     <>
-      <img
-        style={{ width: "80px" }}
-        src="https://cdn.builder.io/api/v1/image/assets%2F66fa4205ed5a4558a776cf72c5db088b%2F19a91240c07c4229bbfff8458cc79a75"
-        alt=""
-      />
+      <title>RestLab | Documents</title>
+      <link rel="icon" href="/favicon.ico" />
+      <meta name="theme-color" content="#081910" />
+      <meta name="description" content="Official documentation for RestLab — explore features, setup guides, and API references." />
     </>
   ),
+  logo: (
+    <img
+      style={{ width: "80px" }}
+      src="https://cdn.builder.io/api/v1/image/assets%2F66fa4205ed5a4558a776cf72c5db088b%2F19a91240c07c4229bbfff8458cc79a75"
+      alt="RestLab Logo"
+    />
+  ),
   project: {
-    link: "https://github.com/shuding/nextra-docs-template",
+    link: "https://github.com/sainaina/RestLab_Docu.git",
   },
-  chat: {
-    link: "https://www.youtube.com/@JBWEBDEVELOPER",
-    icon: <></>,
-  },
+  docsRepositoryBase: "https://github.com/sainaina/RestLab_Docu.git",
 };
+
 export default config;
